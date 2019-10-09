ba54834083d99ad9f0bfc2c21be563cb9bc6d6a9
remi.lamiaux <remi.lamiaux@telecom-sudparis.eu>
2019-10-09 11:29:28 +0200

    ajout de la route de la liste des regions vers la page d'une region

--- src/Controller/RegionController.php
+++ src/Controller/RegionController.php
@@ -3,13 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Region;
+use App\Entity\Room;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegionController extends AbstractController
 {
     /**
-     * @Route("/region", name="region")
+     * @Route("/region", name="region_index")
      */
     public function index()
     {
@@ -18,5 +20,19 @@ class RegionController extends AbstractController
         return $this->render('region/index.html.twig', [
             'regions_list' => $regions,
         ]);
+    }
+
+    /**
+     * @Route("/region/{id}", name="region")
+     * @param int $id
+     * @return Response
+     */
+    public function listRooms(int $id){
+        $region = $this->getDoctrine()->getRepository(Region::class)->find($id);
+        $rooms = $region->getRooms();
+        return $this->render('region/rooms.html.twig', [
+            'region_name' =>$region->getName(),
+            'rooms_list' => $rooms,
+            ]);
     }
 }
