8bcc1143edb6ef2ce333b5ccd75d32e90b865eb4
remi.lamiaux <remi.lamiaux@telecom-sudparis.eu>
2019-11-21 10:40:03 +0100

    la modification dynamique des roles devrait être prise en compte

---
+++ src/Entity/UserVoter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Security\Core\Authorization\Voter\RoleVoter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserVoter extends RoleVoter
+{
+    protected function extractRoles(TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        // si le user existe vraiment, on utilise la méthofe getRole, sinon on retourne un tableau vide
+        return $user instanceof UserInterface ?  $user->getRoles() : [];
+    }
+}
